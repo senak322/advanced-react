5bdc0a6e2b1e12fa4fceb9634cf77a00b1cfb8e8
Aleksandr Viznyi <senak98@yandex.ru>
2023-12-12 19:05:14 +0700

    add redux

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,9 @@
+import { useSearchUsersQuery } from "../store/github/github.api"
+
 export function HomePage() {
+
+    const {isLoading, isError, data} = useSearchUsersQuery()
+
     return (
         <div>HomePage</div>
     )
